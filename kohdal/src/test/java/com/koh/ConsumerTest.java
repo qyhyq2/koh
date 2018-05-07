c08f8780c774ba43af33dc7763cc05afacebe5d2
qyh <qyh@qyhs-Mac-mini.local>
2018-05-07 18:30:54 +0800

    commi

--- kohdal/src/test/java/com/koh/ConsumerTest.java
+++ kohdal/src/test/java/com/koh/ConsumerTest.java
@@ -2,8 +2,13 @@ package com.koh;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.koh.thrift.TestService;
+import org.apache.thrift.TException;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ConsumerTest extends AbstractServiceTest {
 
@@ -25,6 +30,35 @@ public class ConsumerTest extends AbstractServiceTest {
         for (int i = 0; i < max; i++) {
             testService.getById((byte) 1);
         }
+
+        long end = System.currentTimeMillis();
+        long elapsedMilliseconds = end - start;
+
+        System.out.println(String.format("%d次RPC调用(dubbo协议),共耗时%d毫秒,平均%f/秒", max, elapsedMilliseconds, max / (elapsedMilliseconds / 1000.0F)));
+    }
+
+
+    @Test
+    public void testPerm() throws Exception {
+        int max = 5000;
+        System.out.println("dubbo RPC testing => ");
+        ExecutorService threadPool = Executors.newFixedThreadPool(50);
+        CountDownLatch latch = new CountDownLatch(max);
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < max; i++) {
+            threadPool.submit(() -> {
+                try {
+                    testService.getById((byte) 1);
+                    latch.countDown();
+                } catch (TException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        latch.await();
 
         long end = System.currentTimeMillis();
         long elapsedMilliseconds = end - start;
